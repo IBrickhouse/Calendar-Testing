47833dd4c02afc3bd65d7dd4e9441b87b6dc4344
IBrickhouse <mrfurbyb@gmail.com>
2022-06-23 20:16:23 -0700

    Add Print Member Birthday Month Calendar
    Add print member birthday month calendar function

--- cal.py
+++ cal.py
@@ -1,7 +1,11 @@
+# import statements
 import calendar
 from datetime import date
 from xml.etree.ElementTree import tostring
 import twiceMembers
+
+# varablie definitions
+currentYear = date.today().year
 
 def getMembersBirthday(member):
     birthday = twiceMembers.getTwice(member)["Birthday"]
@@ -10,13 +14,25 @@ def getMembersBirthday(member):
 
 def getMembersBirthdayMonth(member):
     birthday = twiceMembers.getTwice(member)["Birthday"]
-    month = str(birthday)
-    month = month[4:6]
-    print(month)
+    bdayMonth = str(birthday)
+    bdayMonth = bdayMonth[4:6]
+    if bdayMonth[0] == '0':
+        bdayMonth = bdayMonth[1]
+        print(int(bdayMonth))
+        return int(bdayMonth)
+        #print(type(bdayMonth))
+    else:
+        print(int(bdayMonth))
+        return int(bdayMonth)
+        #print(type(bdayMonth))
 
-
-
+def showBdayMonCal(member):
+    monCal = calendar.month(currentYear,int(getMembersBirthdayMonth(member)))
+    print(monCal)
 
 
 #getMembersBirthday("Momo")
-getMembersBirthdayMonth("Jihyo")
+#getMembersBirthdayMonth("Jihyo")
+showBdayMonCal("Jihyo")
+
+#calendar.month(00,)
